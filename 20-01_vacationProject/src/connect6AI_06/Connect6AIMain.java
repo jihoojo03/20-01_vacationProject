719e06fdc938006675aa9fa8f78d8cfad67d8f0c
Jeong Ji-Hoon <59548055+jihoojo03@users.noreply.github.com>
2020-10-10 15:12:44 +0900

    For_festival

---
+++ 20-01_vacationProject/src/connect6AI_06/Connect6AIMain.java
@@ -0,0 +1,14 @@
+package connect6AI_06;
+
+
+public class Connect6AIMain {
+
+	public static final int SCREEN_WIDTH = 960;
+	public static final int SCREEN_HEIGHT = 640;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GUITest();
+	}
+
+}
